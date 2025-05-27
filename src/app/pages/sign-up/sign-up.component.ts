f3972fbe2b9affd9b3081b5d2fa0c422171a9b49
jofagaca69 <jfgarciac@unal.edu.co>
2025-05-26 22:35:12 -0500

    Ajustes de estilo y creación del componente de registro

---
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-sign-up',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
+})
+export class SignUpComponent {
+
+}
