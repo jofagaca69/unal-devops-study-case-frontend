76f3d70e853bf9082f7323c3c0f10bd9d0787145
yarenas <ysasarenas@gmail.com>
2025-06-14 02:57:55 -0500

    fixes

--- app/src/app/pages/login/login.component.ts
+++ app/src/app/pages/login/login.component.ts
@@ -29,6 +29,7 @@ export class LoginComponent {
     const { email, password } = this.loginForm.value;
     this.authService.login(email!, password!).subscribe({
       next: (result: any) => {
+        console.log({ result });
         this.errorMsg = '';
         if (result.success) {
           localStorage.setItem('name', result.user.first_name);
